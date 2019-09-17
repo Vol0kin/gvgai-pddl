28b8bd1263c4bb6aa0d87e96ffcc5d168984967e
Vladislav Nikolov <vladis890@gmail.com>
2019-09-17 18:29:37 +0200

    :sparkles: Added method to get variables from predicates. :poop: Started method to parse to PDDL.

--- GVGAI/src/parsing/Parser.java
+++ GVGAI/src/parsing/Parser.java
@@ -13,6 +13,8 @@ import tools.com.google.gson.stream.JsonReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
 // Import IO
 import java.io.FileReader;
@@ -54,7 +56,7 @@ public class Parser {
     /**
      * Method that transforms a given state of the current game to a 2D matrix
      * of String, which represents the game's map.
-     * 
+     *
      * @param so Observation of the current game state.
      * @return Returns a 2D String matrix containing the current state of the game.
      */
@@ -69,13 +71,17 @@ public class Parser {
         // Create a new matrix, representing the game's map
         String[][] gameStringMap = new String[X_MAX][Y_MAX];
 
-        // Iterate over the map and transform it to string
-        // The VGDLRegistry contains information that will allow us to transform
-        // the current StateObservation to a matrix
+        /*
+         * Iterate over the map and transform it to string
+         * The VGDLRegistry contains information that will allow us to transform
+         * the current StateObservation to a matrix
+         */
         for (int y = 0; y < Y_MAX; y++) {
             for (int x = 0; x < X_MAX; x++) {
-                // If there's an observation, get its name using the itype
-                // information
+                /*
+                 * If there's an observation, get its name using the itype
+                 * information
+                 */
                 if (gameState[x][y].size() > 0) {
                     int itype = gameState[x][y].get(0).itype;
                     gameStringMap[x][y] = VGDLRegistry.GetInstance().getRegisteredSpriteKey(itype);
@@ -93,5 +99,80 @@ public class Parser {
         }*/
 
         return gameStringMap;
+    }
+
+    /**
+     * Method that obtains the variables that can be found in the predicates
+     * associated to each of the game elements.
+     *
+     * @param correspondence Map that contains the correspondence between the
+     *                       game element and the predicates associated to it.
+     * @param variables Set of variables that the user has defined.
+     * @return Returns a Map that associates game elements and variables.
+     */
+    public static Map<String, Set<String>> getVariablesFromPredicates(Map<String, ArrayList<String>> correspondence,
+                                                                      Set<String> variables)
+    {
+        /*
+         * Create a map that will contain the game element and the variables
+         * associated to the predicates for that element
+         */
+        Map<String, Set<String>> varsFromPredicates = new HashMap<>();
+
+        // Iterate over the correspondence map
+        for (Map.Entry<String, ArrayList<String>> entry : correspondence.entrySet()) {
+            // Get the key, which represent the game element
+            String key = entry.getKey();
+
+            // Create a new set of contained variables in the predicates
+            Set<String> containedVars = new HashSet<>();
+
+            /*
+             * Iterate over each predicate and each variable to find
+             * which ones of the variables are contained in the predicates
+             * corresponding to the game element and add them to the set
+             */
+            for (String predicate : entry.getValue()) {
+                for (String var : variables) {
+                    if (predicate.contains(var)) {
+                        containedVars.add(var);
+                    }
+                }
+            }
+
+            // Update the map entry
+            varsFromPredicates.put(key, containedVars);
+        }
+
+        return varsFromPredicates;
+    }
+
+    public static void parseGameToPDDL(String[][] gameMap, Map<String, String> variables,
+                                       Map<String, ArrayList<String>> predicates,
+                                       Map<String, String> connections)
+    {
+        Map<String, Integer> numVariables = new HashMap<>();
+
+        for (String key: variables.keySet()) {
+            numVariables.put(key, 0);
+        }
+
+        ArrayList<String> predicateList = new ArrayList<>();
+        ArrayList<String> connectionList = new ArrayList<>();
+
+        final int X_MAX = gameMap.length, Y_MAX = gameMap[0].length;
+
+        for (int y = 0; y < Y_MAX; y++) {
+            for (int x = 0; x < X_MAX; x++) {
+                String cellType = gameMap[x][y];
+
+                if (predicates.containsKey(cellType)) {
+                    // Increase the number of variables from that type
+                    numVariables.replace(cellType, numVariables.get(cellType) + 1);
+
+
+                }
+            }
+        }
     }
 }
