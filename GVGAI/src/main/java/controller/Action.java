1bbe83c81ca3f7bc932a75c9ea663cbd1664bc67
Vladislav Nikolov <vladis890@gmail.com>
2020-02-04 14:36:44 +0100

    Added method to get all the preconditions of a given action #2

--- GVGAI/src/main/java/controller/Action.java
+++ GVGAI/src/main/java/controller/Action.java
@@ -1,19 +1,55 @@
 package controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.*;
+import java.util.ArrayList;
+
 public class Action {
     private String actionName;
-    private String preconditions;
+    private List<String> preconditions;
 
-    public Action(String actionName, String preconditions) {
+    public Action(String actionName, String actionDescription) {
         this.actionName = actionName;
-        this.preconditions = preconditions;
+        this.processPreconditionsFromActionDescription(actionDescription);
+    }
+
+    private void processPreconditionsFromActionDescription(String action) {
+        // Crete pattern
+        Pattern preconditionPattern = Pattern.compile(":precondition[^:]+");
+
+        // Get match of the pattern
+        Matcher preconditionMatcher = preconditionPattern.matcher(action);
+        // Should this call be controlled in case no match is found?
+        preconditionMatcher.find();
+
+        // The preconditions are in the first match since there's only one
+        String preconditionsMatch = preconditionMatcher.group(0);
+
+        // Remove all extra spaces, spaces between consequent parentheses and ":precondition" at the beginning
+        preconditionsMatch = preconditionsMatch.replaceAll("\\s+", " ");
+        preconditionsMatch = preconditionsMatch.substring(0, preconditionsMatch.length() - 1);
+        preconditionsMatch = preconditionsMatch.replaceAll(" \\)", ")");
+        preconditionsMatch = preconditionsMatch.replaceAll(":precondition\\s+", "");
+
+        // Remove and statement, if there's any. Also removes last parenthesis
+        if (preconditionsMatch.contains("and")) {
+            preconditionsMatch = preconditionsMatch.replaceFirst("\\(and ", "");
+            preconditionsMatch = preconditionsMatch.substring(0, preconditionsMatch.length() - 1);
+        }
+
+        // Split preconditions in different lines
+        preconditionsMatch = preconditionsMatch.replaceAll("\\) \\(", "\\)\n\\(");
+
+        // Get preconditions as a list
+        this.preconditions = new ArrayList<>(Arrays.asList(preconditionsMatch.split("\n")));
     }
 
     public String getActionName() {
         return this.actionName;
     }
 
-    public String getPreconditions() {
+    public List<String> getPreconditions() {
         return this.preconditions;
     }
 
